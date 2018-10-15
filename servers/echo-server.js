39beaf9242a854bcd293254cc178fda8151ab3b9
Robert Flack <flackr@gmail.com>
2018-10-15 10:05:32 -0400

    Allow server to not reply to old messages.

--- servers/echo-server.js
+++ servers/echo-server.js
@@ -1,8 +1,29 @@
+if (typeof exports !== 'undefined') {
+this.performance = require('perf_hooks').performance;
+}
+
 (function(exports, scope) {
 
   function accept(sourceSocket) {
+    let lastMessage = undefined;
+    let timeDelta = undefined;
+    let skipped = 0;
+    const GARBAGE_SEPARATOR = '{(})';
     sourceSocket.addEventListener('message', function(evt) {
-      sourceSocket.send(evt.data);
+      let garbage_start = evt.data.indexOf(GARBAGE_SEPARATOR);
+      let data = JSON.parse(evt.data.substring(0, garbage_start));
+      // Only reply to messages in order
+      if (lastMessage && lastMessage.i > data.i) {
+        skipped++;
+        return;
+      }
+      let serverNow = scope.performance.now();
+      if (!timeDelta)
+        timeDelta = data.time - serverNow;
+      data.serverTime = serverNow + timeDelta;
+      data.skipped = skipped;
+      sourceSocket.send(JSON.stringify(data) + evt.data.substring(garbage_start));
+      lastMessage = data;
     })
   }
 
